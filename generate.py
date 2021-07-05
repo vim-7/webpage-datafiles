2b121be6f7ca6301a23fe9165401bf3079ecdef6
kumitatepazuru <riku.ueda@outlook.jp>
2021-07-05 16:55:12 +0900

    webページ開設のお知らせを追加

---
+++ generate.py
@@ -0,0 +1,42 @@
+import glob
+import json
+import markdown
+import os
+import shutil
+from datetime import datetime
+from markdown.extensions.codehilite import CodeHiliteExtension
+
+pypath = os.path.dirname(os.path.abspath(__file__))
+md_path = input("ファイル名を指定 >")
+with open(md_path) as f:
+    md = markdown.Markdown(extensions=["extra", CodeHiliteExtension()])
+    result = md.convert(f.read())
+
+t = input("1 お知らせ\n2 ブログ\n> ")
+if t == "1":
+    title = input("タイトルを入力> ")
+    author = input("あなたの名前を入力> ")
+    with open(pypath + "/tags.json", "r") as f:
+        tag_data = json.load(f)
+        tag = tag_data[int(input(
+            "\n".join(list(map(lambda n: str(n[0] + 1) + " " + n[1], enumerate(tag_data))))
+            + "\n> ")) - 1]
+    id = len(glob.glob(pypath + "/news/*"))
+    path = pypath + "/news/" + str(id) + "/"
+    os.makedirs(path, exist_ok=True)
+    shutil.copyfile(md_path, path + "/contents.md")
+    with open(path + "contents.html", "w") as f:
+        f.write(result)
+    with open(pypath + "/news/index.json", "r") as f:
+        data = json.load(f)
+    data[tag].append({"date": datetime.now().strftime("%Y/%m/%d"), "title": title})
+    with open(pypath + "/news/index.json", "w") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+    data = {
+        "author": author,
+        "update": datetime.now().strftime("%Y/%m/%d"),
+        "title": title,
+        "info": ["news", tag]
+    }
+    with open(path + "/data.json", "w") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
