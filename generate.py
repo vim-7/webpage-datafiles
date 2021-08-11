873050907e96ff40d38a6ae1745244df02cae7ef
kumitatepazuru <riku.ueda@outlook.jp>
2021-08-12 08:16:29 +0900

    別ジャンルのテスト投稿（確認次第削除）

--- generate.py
+++ generate.py
@@ -17,10 +17,23 @@ if t == "1":
     title = input("タイトルを入力> ")
     author = input("あなたの名前を入力> ")
     with open(pypath + "/tags.json", "r") as f:
-        tag_data = json.load(f)["news"]
-        tag = tag_data[int(input(
-            "\n".join(list(map(lambda n: str(n[0] + 1) + " " + n[1], enumerate(tag_data))))
-            + "\n> ")) - 1]
+        data = json.load(f)
+    tag_data = ["news"]
+    l = int(input("カテゴリ名を指定\n" +
+                  "\n".join(list(map(lambda n: str(n[0] + 1) + " " + n[1], enumerate(tag_data))))
+                  + "\n" + str(len(tag_data) + 1) + " [新規作成]\n> ")) - 1
+    if len(tag_data) > l:
+        tag = tag_data[l]
+    else:
+        tag = input("カテゴリ名 > ")
+        with open(pypath + "/tags.json", "w") as f:
+            data["news"].append(tag)
+            json.dump(data, f, indent=2, ensure_ascii=False)
+        with open(pypath + "/news/index.json", "r") as f:
+            data = json.load(f)
+        data.append({"name":tag,"content":[]})
+        with open(pypath + "/news/index.json", "w") as f:
+            json.dump(data,f, indent=2, ensure_ascii=False)
     id = len(glob.glob(pypath + "/news/*"))
     path = pypath + "/news/" + str(id) + "/"
     os.makedirs(path, exist_ok=True)
@@ -29,14 +42,15 @@ if t == "1":
         f.write(result)
     with open(pypath + "/news/index.json", "r") as f:
         data = json.load(f)
-    data[list(map(lambda n:n["name"],data)).index(tag)]["content"].append({"date": datetime.now().strftime("%Y/%m/%d"), "title": title, "id": id})
+    data[list(map(lambda n: n["name"], data)).index(tag)]["content"].append(
+        {"date": datetime.now().strftime("%Y/%m/%d"), "title": title, "id": id})
     with open(pypath + "/news/index.json", "w") as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
     data = {
         "author": author,
         "update": datetime.now().strftime("%Y/%m/%d"),
         "title": title,
-        "info": ["news", tag]
+        "info": ["ニュース", tag]
     }
     with open(path + "/data.json", "w") as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
